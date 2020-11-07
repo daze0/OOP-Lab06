96e00defc10f8447cb6e6c55bb9744dd33f93ef9
daze <leon.baiocchi@studio.unibo.it>
2020-11-07 17:41:36 +0000

    do first part ex2

--- src/it/unibo/oop/lab/collections1/UseCollection.java
+++ src/it/unibo/oop/lab/collections1/UseCollection.java
@@ -1,11 +1,19 @@
 package it.unibo.oop.lab.collections1;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Example class using {@link java.util.List} and {@link java.util.Map}.
  * 
  */
 public final class UseCollection {
 
+	private static final int FIRST = 0;
+	private static final int ADJUST_LAST = 1;
+
+	
     private UseCollection() {
     }
 
@@ -18,19 +26,30 @@ public final class UseCollection {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+    	List<Integer> arrayList = new ArrayList<>();
+    	for(int i = 1000; i < 2000; i++) {
+    		arrayList.add(i);
+    	}
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+    	List<Integer> linkedList = new LinkedList<>(arrayList);
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+    	int temporaryVariable = arrayList.get(FIRST);
+    	arrayList.set(FIRST, arrayList.get(arrayList.size() - ADJUST_LAST));
+    	arrayList.set(arrayList.size() - ADJUST_LAST, temporaryVariable);
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+    	for(int elem : arrayList) {
+    		System.out.println(elem);
+    	}
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -38,6 +57,7 @@ public final class UseCollection {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+    	
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
